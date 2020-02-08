8bda2bb0f3dba8672455cbad6824513f66c9b25a
jordantvincent <46661671+jordantvincent@users.noreply.github.com>
2020-02-08 09:42:33 -0500

    Stock & Portfolio Classes

--- app/src/main/java/edu/ggc/jcantillo/blackrock/Stock.java
+++ app/src/main/java/edu/ggc/jcantillo/blackrock/Stock.java
@@ -1,22 +1,34 @@
 package edu.ggc.jcantillo.blackrock;
-import java.util.Date;
-
-
 public class Stock {
     private String purchaseDate;
     private String sellDate;
     private double purchaseValue;
-    private double currentStockValue;
-    private int quantityOwned;
     private String stockName;
+    private int quantityOwned;
 
-
-    public Stock(String stockName, double purchaseValue){
-        purchaseDate = new Date()+"";
-        this.stockName = stockName;
-        this.purchaseValue = purchaseValue;
-        this.sellDate = "";
-        this.currentStockValue
-
+    public Stock(String stockAbreviation){
+        this.stockName = stockAbreviation;
+        purchaseDate = "5:19am";
+        purchaseValue = getCurrentValue(stockAbreviation);
+        quantityOwned = 0;
     }
+
+    public double getCurrentValue(String stockAbreviation){
+        //THIS METHOD WOULD ACCESS THE API AND RETURN THE CURRENT VALUE
+        double returnedValue = 250.0;
+        return returnedValue;
+    }
+
+    public String getstockName() {
+        return stockName;
+    }
+
+    public void setQuantityOwned(int quantityOwned) {
+        quantityOwned = this.quantityOwned;
+    }
+
+    public int getQuantityOwned(){
+        return quantityOwned;
+    }
+
 }
