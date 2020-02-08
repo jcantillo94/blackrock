4c8e9514b4ab484d3240fe3987ecb33a23f537c2
jordantvincent <46661671+jordantvincent@users.noreply.github.com>
2020-02-08 16:46:12 -0500

    Revert "Stock & Portfolio Classes"
    This reverts commit 8bda2bb0f3dba8672455cbad6824513f66c9b25a.

--- app/src/main/java/edu/ggc/jcantillo/blackrock/Portfolio.java
+++ app/src/main/java/edu/ggc/jcantillo/blackrock/Portfolio.java
@@ -2,71 +2,12 @@ package edu.ggc.jcantillo.blackrock;
 import java.util.ArrayList;
 
 public class Portfolio {
-    private ArrayList<Stock> stocksOwned = new ArrayList<Stock>();
-    private ArrayList<Stock> stocksSold;
+
+    private double portfolioValue;
     private double cashValue;
     private double stocksValue;
-    private double startingValue;
-    private double portfolioValue;
+    private Stock[] stocks;
 
 
-    public Portfolio(double initialInvestment){
-        this.startingValue = initialInvestment;
-        this.cashValue = initialInvestment;
-        this.stocksValue = 0;
-        this.portfolioValue = cashValue + stocksValue;
-
-    }
-
-    public void purchaseStock(String stockName, int quantity){
-
-        System.out.println("Quantity " + quantity);
-
-        //temporary Stock variable to hold the stock information
-        Stock tempStock = new Stock(stockName);
-        tempStock.setQuantityOwned(quantity);
-        System.out.println(tempStock.getQuantityOwned());
-
-        //calculates transaction cost based on current price of stock and quantity
-        double purchaseCost = tempStock.getCurrentValue(stockName) * quantity;
-
-        if (purchaseCost <= startingValue) {
-
-
-            stocksOwned.add(tempStock); //add it to stock list
-            cashValue -= purchaseCost;
-            stocksValue += purchaseCost;
-
-            System.out.println("Stock purchased!");
-        }
-
-        else {
-            System.out.println("You don't have the funds to purchase that many stocks.  Try again.");
-        }
-
-
-    }
-
-
-    public ArrayList<Stock> getStocksOwned(){
-        return stocksOwned;
-    }
-
-    public double getCashValue() {
-        return cashValue;
-    }
-
-    public double getStartingValue() {
-        return startingValue;
-    }
-
-    public double getStocksValue() {
-        return stocksValue;
-    }
-
-    public double calculatePortfolioPercentIncrease(){
-        double perfectIncrease = 1.0 - (startingValue / portfolioValue);
-        return perfectIncrease;
-    }
 
 }
